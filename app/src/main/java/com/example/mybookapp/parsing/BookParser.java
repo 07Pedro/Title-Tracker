648cc2dbab2736e2bcbc6a04ef877a924a9ebdca
Petr Cerny <petr.cerny@e3ag.ch>
2024-12-18 09:04:05 +0100

    fix value parsing

--- app/src/main/java/com/example/mybookapp/parsing/BookParser.java
+++ app/src/main/java/com/example/mybookapp/parsing/BookParser.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BookParser {
@@ -27,21 +28,41 @@ public class BookParser {
                             bookNode.get("first_publish_year").asInt() : -1;
                     String title = bookNode.has("title") ?
                             bookNode.get("title").asText() : "Unknown Title";
-                    String subject = bookNode.has("subject") ?
-                            bookNode.get("subject").toString() : "No Subject";
+                    List<String> subjectList = new ArrayList<>();
+                    if (bookNode.has("subject") && bookNode.get("subject").isArray()) {
+                        bookNode.get("subject").forEach(node -> subjectList.add(node.asText()));
+                    }
+                    String randomSubjects = selectRandomSubjects(subjectList, 3);
+
                     String leadingISBN = null;
                     if (bookNode.has("isbn") && bookNode.get("isbn").isArray() && !bookNode.get("isbn").isEmpty()) {
                         leadingISBN = bookNode.get("isbn").get(0).asText();
                     }
-                    String firstSentence = bookNode.has("first_sentence") ?
-                            bookNode.get("first_sentence").toString() : null;
+                    String firstSentence = null;
+                    if (bookNode.has("first_sentence") && bookNode.get("first_sentence").isArray()) {
+                        JsonNode firstSentenceNode = bookNode.get("first_sentence").get(0);
+                        if (firstSentenceNode != null) {
+                            firstSentence = firstSentenceNode.asText();
+                        }
+                    }
 
-                    books.add(new Book(authorAlternativeName, authorName, firstPublishYear, title, subject, leadingISBN, firstSentence));
+                    books.add(new Book(authorAlternativeName, authorName, firstPublishYear, title, randomSubjects, leadingISBN, firstSentence));
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return books;
+    }
+
+    private static String selectRandomSubjects(List<String> subjects, int limit) {
+        if (subjects == null || subjects.isEmpty()) {
+            return "No Subject";
+        }
+        Collections.shuffle(subjects);
+        return subjects.stream()
+                .limit(limit)
+                .reduce((s1, s2) -> s1 + ", " + s2)
+                .orElse("No Subject");
     }
 }
